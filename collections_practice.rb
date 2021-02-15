bd67f234ea5e5ec78081b620573fdfb51e90d0c5
rheadp <rheadpea@msu.edu>
2021-02-15 11:25:21 -0500

    commit tree 2d88db8545198d8ccb101258097f32b85596b638

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort!
+    array.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        a.length <=> b.length end
+end
+
+def swap_elements(array)
+    array[-2], array[-1] = array[-1], array[-2]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.collect do |third|
+        third[2] = "$"
+        third
+    end
+end
+
+def find_a(array)
+    array.select do |i|
+        i.chr == "a"
+    end
+end
+
+def sum_array(array)
+    array.inject do |sum, num|
+        sum + num end
+end
+
+def add_s(array)
+    array.each_with_index.collect do |element, index|
+        if index == 1
+            element
+        else 
+            element + "s"
+        end
+    end
+end
